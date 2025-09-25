c3b662c8d7341ea4f9da125bf25e0d9ef7691fbc
Alok Panchal <alokpanchal27@gmail.com>
2025-09-26 00:34:40 +0530

    Initial Commit

---
+++ src/main/java/com/marble/IMarbleApplication.java
@@ -0,0 +1,13 @@
+package com.marble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMarbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMarbleApplication.class, args);
+	}
+
+}
