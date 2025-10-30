2fde188145bb992931f009744c39808e67526ac7
Ritikaa-12 <malviyaritika41@gmail.com>
2025-10-30 16:42:41 +0530

    staffPayment

--- src/main/java/com/marble/dto/StaffSalaryDto.java
+++ src/main/java/com/marble/dto/StaffSalaryDto.java
@@ -6,13 +6,13 @@ import lombok.Data;
 
 @Data
 public class StaffSalaryDto {
-private Integer salaryId;
-
-	private Integer staff;
-	private String month;
-	private String year;
-	private Float amount;
-	private LocalDate paymentDate;
-	private String paymentMode;
+    private Integer salaryId;
+    private Integer staffId;
+    private String month;
+    private String year;
+    private Float amount;
+    private LocalDate paymentDate;
+    private String paymentMode;
     private String description;
 }
+
